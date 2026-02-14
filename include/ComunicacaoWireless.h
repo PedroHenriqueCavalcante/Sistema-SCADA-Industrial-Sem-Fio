49ec2ae17bd34d15401b1f978d25ce644accc9a2
PedroHenriqueCavalcante <pedropedro.phsc@gmail.com>
2026-02-14 13:58:00 -0300

    Adiciona um novo arquivo .h para o envio dos arquivos via comunicação wireless. Adicona a lógica de comunicação wireless na main

---
+++ include/ComunicacaoWireless.h
@@ -0,0 +1,50 @@
+#ifndef RADIO_TRANSMISSOR_H
+#define RADIO_TRANSMISSOR_H
+
+#include <Arduino.h>
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+
+
+struct PacoteDados { //Scruct que vai ser enviada para o ESP8266
+    char idSensor[15]; // Nome do sensor (ex: "Temp_Interna")
+    float valor;       // O valor lido (ex: 45.5)
+    char unidade[5];   // Unidade (ex: "C", "RPM")
+};
+
+class RadioTransmissor {
+    private:
+        RF24 radio; // Objeto da biblioteca oficial
+        const uint64_t enderecoPipe = 0x1234567890LL; // Endereço de envio
+
+    public:
+
+        RadioTransmissor(int pinCE, int pinCSN);
+        void begin();
+        bool enviar(PacoteDados dados);
+};
+
+RadioTransmissor::RadioTransmissor(int pinCE, int pinCSN):radio(pinCE, pinCSN) { // Construtor: Inicializa o objeto RF24 com os pinos CE e CSN
+
+}
+
+void RadioTransmissor::begin() {
+
+    radio.begin(); 
+    radio.setChannel(5); 
+    radio.setDataRate(RF24_2MBPS);
+    radio.setPALevel(RF24_PA_HIGH);
+    
+    radio.openWritingPipe(enderecoPipe); // Abre o tubo para escrita (Tx)
+    
+    radio.stopListening(); // O transmissor não ouve, ele só fala
+}
+
+bool RadioTransmissor::enviar(PacoteDados dados) {
+            
+    return radio.write(&dados, sizeof(PacoteDados)); // Retorna 'true' se o envio foi recebido com sucesso (ACK)
+
+}
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <Arduino.h>
 #include "ClassesSCADA.h"
+#include "ComunicacaoWireless.h"
 
 // Pinos dos Sensores
 #define PINO_ONE_WIRE  4  // Fio de dados dos DS18B20
@@ -8,63 +9,91 @@
 #define PINO_LDR_1     A0 // LDR 1
 #define PINO_LDR_2     A1 // LDR 2
 
+// Pinos do Rádio NRF24L01 (NO ARDUINO)
+#define PINO_CE  9
+#define PINO_CSN 10
+
 #define TIPO_DHT DHT22 
 
 OneWire oneWire(PINO_ONE_WIRE);
 DallasTemperature sensors(&oneWire);
 DHT dht(PINO_DHT, TIPO_DHT);
 
-// Endereços dos sensores DS18B20. MUDAR DEPOIS!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!<<<<<-----------
+// Instância do Rádio
+RadioTransmissor meuRadio(PINO_CE, PINO_CSN);
+
+// Endereços dos sensores DS18B20 (Lembrar de colocar os que Eugênio enviar aqui)
 DeviceAddress enderecoSensorTemp1 = { 0x28, 0xFF, 0x12, 0x34, 0x56, 0x78, 0x9A, 0xBC };
 DeviceAddress enderecoSensorTemp2 = { 0x28, 0xAA, 0xBB, 0xCC, 0xDD, 0xEE, 0xFF, 0x11 };
 
-
-// Array de ponteiros para a classe mãe. Tamanho 6 pois são 6 sensores.
+// Array de ponteiros para a classe mãe. São 6 sensores
 Sensores* listaSensores[6]; 
 
 void setup() {
   Serial.begin(9600);
   Serial.println("INICIANDO SISTEMA SCADA");
 
+  // Inicializa Hardware dos Sensores
   sensors.begin();
   dht.begin();
 
-  // 1. Sensor Óptico (RPM)
+  // Inicializa o Rádio
+  Serial.print("Iniciando Radio... ");
+  meuRadio.begin();
+  Serial.println("OK!");
+
+  // INSTANCIAÇÃO (Polimorfismo)
+
+  //Sensor Óptico
   listaSensores[0] = new TCRT5000("Cooler", "Externo", PINO_OPTICO);
 
-  // 2. Sensor DHT (Temperatura e Umidade)
+  //Sensor DHT
   listaSensores[1] = new SensorDHT("Umidade_Sala", "Externo", &dht);
 
-  // 3. Sensor Temperatura DS18B20 #1
+  //Sensor Temperatura DS18B20 #1
   listaSensores[2] = new DS18B20("Temp_Interna", "Interno", &sensors, enderecoSensorTemp1);
 
-  // 4. Sensor Temperatura DS18B20 #2
+  //Sensor Temperatura DS18B20 #2
   listaSensores[3] = new DS18B20("Temp_Externa", "Externo", &sensors, enderecoSensorTemp2);
 
-  // 5. Sensor LDR #1
+  //Sensor LDR #1
   listaSensores[4] = new LDR("Luz_Ambiente", "Externo", PINO_LDR_1);
 
-  // 6. Sensor LDR #2
+  //Sensor LDR #2
   listaSensores[5] = new LDR("Luz_Interna", "Interno", PINO_LDR_2);
 }
 
 void loop() {
   Serial.println("\n--- Nova leitura iniciando ---");
 
-  // Loop Polimórfico: Trata todos os sensores igual, não importa o tipo
   for(int i = 0; i < 6; i++) {
     
-    
-    listaSensores[i]->ler();// 1. Manda ler todos os sensores. Cada um usa sua própria lógica definida no header (ClassesSCADA.h)
+    listaSensores[i]->ler(); // 1. Manda cada sensor ler
 
-    // 2. Imprime o relatório
-    // Nota: O DHT vai imprimir apenas a temperatura aqui, a menos que você tenha sobrescrito o getRelatorio() na classe dele para mostrar umidade.
+    // 2. Imprime o relatório no Serial (Para Debug)
     Serial.println(listaSensores[i]->getRelatorio());
     
-    // Pequeno delay para não travar o serial
-    delay(100);
+    PacoteDados pacote;
+
+    // A. Converte o ID (String) para char array (necessário para o rádio)
+    // O '15' é o tamanho máximo que definimos na struct no arquivo .h
+    listaSensores[i]->getId().toCharArray(pacote.idSensor, 15);
+
+    // B. Pega o valor numérico (float)
+    pacote.valor = listaSensores[i]->getValor();
+
+    // C. Envia
+    Serial.print("-> Enviando via Radio... ");
+    if (meuRadio.enviar(pacote)) {
+        Serial.println("Sucesso no envio");
+    } else {
+        Serial.println("Falha no envio (Sem resposta).");
+    }
+
+    // Pequeno delay para não congestionar o rádio entre um sensor e outro
+    delay(250); 
   }
 
-  // Espera 2 segundos para a próxima rodada
+  // Espera 2 segundos para a próxima rodada completa
   delay(2000);
 }
